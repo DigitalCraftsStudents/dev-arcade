4ec5cdac906c3bfc52d86a373bdb64087ca9c816
drlenny <66442932+drlenny@users.noreply.github.com>
2021-06-19 12:54:15 -0500

    add a div to board

--- client/src/games/memory/MemoryGame.jsx
+++ client/src/games/memory/MemoryGame.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Provider } from "react-redux";
+import Board from './components/Board';
 
 const MemoryGame = (props) => (
   <div>
     <h1>Hello from Memory Game! part 2</h1>
+    <Board />
   </div>
 );
 
---
+++ client/src/games/memory/components/Board.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Board(){
+    return (
+        <div style={{background: 'green'}}>
+            cards go here
+        </div>
+    )
+}
+
+export default Board;
