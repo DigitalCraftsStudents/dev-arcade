72f3ead460cb7361adf8fabab99d820eb416e590
coreycarmichaelcoreycarmichaelcleargit config -- global user.email corey.carmichael@gmail.com <77471705+coreycarmichael@users.noreply.github.com>
2021-06-20 22:01:27 -0400

    reorganization

---
+++ client/src/games/memory/MemoryGame.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Provider } from "react-redux";
+
+const MemoryGame = (props) => (
+  <div>
+    <h1>Hello from Memory Game!</h1>
+  </div>
+);
+
+export default MemoryGame;
