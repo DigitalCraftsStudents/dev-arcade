4cfff6cbcfd38b363eb3aa49f4cd78ae2b07f25e
bvoncarr <b.voncarr@gmail.com>
2021-06-22 22:50:32 -0400

    player name input box added, moved score to top, added files

--- client/src/games/Minesweeper/MinesweeperGame.js
+++ client/src/games/Minesweeper/MinesweeperGame.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Board from './components/Board';
 import ScoreBoard from './components/ScoreBoard';
+import SignupForm from './components/SignupForm';
 // import './App.css';
 
 function MinesweeperGame() {
   return (
     <div>
+      <SignupForm />   <ScoreBoard/>
+
       <div className="heading">
       <h1>Mine......Sweeper</h1>
       </div>
       <div className="aligned">   
         <Board/>
-        <ScoreBoard/>
+        
       </div>
     </div>
   );
