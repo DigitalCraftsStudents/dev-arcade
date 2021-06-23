c5b3d3d019156b78093704c2d06b840f0fa1930d
bvoncarr <b.voncarr@gmail.com>
2021-06-22 21:37:48 -0400

    scoreboard function updated

--- client/src/games/Minesweeper/components/ScoreBoard.js
+++ client/src/games/Minesweeper/components/ScoreBoard.js
@@ -6,7 +6,7 @@ export default function ScoreBoard(props) {
             <div>Score:{ props.score }</div>
             <div>Level: 1</div>
             <button className="score-board-button" onClick={(e) => {
-            }}>Play</button>
+            }}>Play Game</button>
             <button className="score-board-button" onClick={(e) => {
             }}>Restart</button>
         </div>
