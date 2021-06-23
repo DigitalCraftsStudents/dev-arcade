e6ab4fb7d3edf0994672e9ce53ceea6f3fad170f
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-06-22 21:35:32 -0400

    trying to render

---
+++ client/src/games/chess/components/MyChessBoard.js
@@ -0,0 +1,13 @@
+// import { Provider } from 'react-redux';
+import ChessBoard from 'chessboardjsx';
+
+// This is App.js from video
+function MyChessBoard() {
+    return (
+<div>
+<ChessBoard position="start"/>
+</div>
+    );
+}
+
+export default MyChessBoard
