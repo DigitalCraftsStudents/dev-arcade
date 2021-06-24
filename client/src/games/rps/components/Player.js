3cb5b1bcc2479bcf120de6796d2924dbada835fd
myrriame <myrriametiffany@gmail.com>
2021-06-24 19:56:55 -0400

    edited images

--- client/src/games/rps/components/Player.js
+++ client/src/games/rps/components/Player.js
@@ -1,5 +1,5 @@
 import React from "react";
-import paper from "./images/paperkinda.png";
+import paper from "./images/paper.png";
 import scissors from "./images/scissors.png";
 import rock from "./images/rock.png";
 
