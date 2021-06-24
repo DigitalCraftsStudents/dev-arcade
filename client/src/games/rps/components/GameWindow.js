5039969cb9c13794beb863fefe2549f2435551f4
myrriame <myrriametiffany@gmail.com>
2021-06-23 21:04:27 -0400

    added button functionality and changed a f component into a class component

--- client/src/games/rps/components/GameWindow.js
+++ client/src/games/rps/components/GameWindow.js
@@ -1,19 +1,38 @@
-function GameBoard() {
-    return (
-        <div id="board"> <div id="top"><span>&nbsp;x&nbsp;</span>&nbsp;</div>
+import React from 'react'
+import Player from "./Player";
 
-            <div id="window">
+const actions=["rock", "paper", "scissors"]
+
+class GameBoard extends React.Component {
+
+    state={
+        playerOne: actions[1]
+    }
+
+    selectAction=(actions) => {
+        this.setState({
+            playerOne: actions
+        })
+    }
+
+    render() {
+        const { playerOne }=this.state
+        return (
+            <div id="board"> <div id="top"><span>&nbsp;x&nbsp;</span>&nbsp;</div>
+
+                <div id="window">
+                    <Player actions={playerOne} />
 
 
-                <div id="pickObject">
-                    <span>ROCK</span>&nbsp;
-                    <span>PAPER</span>&nbsp;
-                    <span>SCISSORS</span>
+                    <div id="pickObject">
+                        <button className="actionBtn" onClick={() => this.selectAction('rock')}>ROCK</button>&nbsp;
+                    <button className="actionBtn" onClick={() => this.selectAction('paper')}>PAPER</button>&nbsp;
+                    <button className="actionBtn" onClick={() => this.selectAction('scissors')}>SCISSORS</button>
+                    </div>
                 </div>
             </div>
-        </div>
 
-    )
+        )
+    }
 }
-
 export default GameBoard
---
+++ client/src/games/rps/components/Player.js
@@ -0,0 +1,19 @@
+import React from "react";
+import paper from "./images/paperkinda.png";
+import scissors from "./images/scissors.png";
+import rock from "./images/rock.png";
+
+const Player=({ actions }) => {
+    return (
+        <span>
+            <img className="action" src={
+                actions==="rock"? rock:actions==="paper"? paper:scissors
+            } alt={
+                actions==="rock"? "rock":actions==="paper"? "paper":"scissors"
+            }></img>
+
+        </span>
+    )
+}
+
+export default Player
