2c3a3a2e85e12b86b1c8ce999120d2566f110731
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-06-24 21:33:00 -0400

    working on board positions

--- client/src/games/chess/components/MyChessBoard.js
+++ client/src/games/chess/components/MyChessBoard.js
@@ -1,11 +1,20 @@
 // import { Provider } from 'react-redux';
 import ChessBoard from 'chessboardjsx';
-
+import React, {useState} from 'react';
 // This is App.js from video
+
+const container = {
+    marginTop: "2rem",
+    display: "flex",
+    justifyContent: "space-around",
+    alightItem: "center"
+}
+
 function MyChessBoard() {
+    const [fen, setFen] = useState("start")
     return (
-<div>
-<ChessBoard position="start"/>
+<div className="MyChessBoard" style={container}>
+<ChessBoard position={fen}/>
 </div>
     );
 }
