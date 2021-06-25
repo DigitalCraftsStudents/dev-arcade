fbea7b05bf59d07995c36c9c27d68ec3973caa58
Eric Treadway <eric.s.treadway@gmail.com>
2021-06-24 19:55:42 -0500

    first commit

--- client/src/games/memory/MemoryGame.jsx
+++ client/src/games/memory/MemoryGame.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Provider } from "react-redux";
-import Board from './components/Board';
-
+import Board from "./components/Board";
+import ScoreBoard from "./components/ScoreBoard";
 
 function MemoryGame() {
   return (
-  <div>
-    <h1>Hello from Memory Game! part 2</h1>
-    <Board />
-  </div>
-  )
-};
-
+    <div>
+      <h1>Hello from Memory Game! part 2</h1>
+      <Board />
+      <ScoreBoard />
+    </div>
+  );
+}
 
 export default MemoryGame;
---
+++ client/src/games/memory/components/ScoreBoard.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+function ScoreBoard() {
+  return <div>Hello Score board</div>;
+}
+
+export default ScoreBoard;
