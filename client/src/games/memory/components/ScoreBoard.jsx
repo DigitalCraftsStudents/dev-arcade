a0efe6eabe3a48b78be89f4bb5bfe45235a3e907
Eric Treadway <eric.s.treadway@gmail.com>
2021-06-25 14:40:28 -0500

    removed decrement because it is not needed at this time

--- client/src/games/memory/components/ScoreBoard.jsx
+++ client/src/games/memory/components/ScoreBoard.jsx
@@ -4,7 +4,6 @@ import { store } from "../store";
 
 import Output from "../containers/OutputContainer";
 import Increment from "../containers/IncrementContainer";
-import Decrement from "../containers/DecrementContainer";
 
 const ScoreBoard = (props) => (
   <Provider store={store}>
@@ -13,7 +12,6 @@ const ScoreBoard = (props) => (
       <div>Score:</div>
       <Output />
       <Increment />
-      <Decrement />
     </div>
   </Provider>
 );
