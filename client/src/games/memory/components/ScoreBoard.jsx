6b0c0a011d0282962b32fb5140a4607755680d15
Eric Treadway <eric.s.treadway@gmail.com>
2021-06-25 14:36:28 -0500

    added redux counter to score board to retool as score and name

--- client/src/games/memory/components/ScoreBoard.jsx
+++ client/src/games/memory/components/ScoreBoard.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import { Provider } from "react-redux";
+import { store } from "../store";
 
-function ScoreBoard() {
-  return (
+import Output from "../containers/OutputContainer";
+import Increment from "../containers/IncrementContainer";
+import Decrement from "../containers/DecrementContainer";
+
+const ScoreBoard = (props) => (
+  <Provider store={store}>
     <div>
       <div>Username:</div>
       <div>Score:</div>
+      <Output />
+      <Increment />
+      <Decrement />
     </div>
-  );
-}
+  </Provider>
+);
 
 export default ScoreBoard;
