0d4c93aac3bfea6debdc7a358868181e1926e8c9
csolis1 <celida.solis@outlook.com>
2021-06-26 12:49:34 -0400

    footer floating head images

--- client/src/games/rps/components/Footer.js
+++ client/src/games/rps/components/Footer.js
@@ -1,9 +1,14 @@
+import myr from '../components/images/myr.png';
+import celi from '../components/images/celi.png';
+import skyler from '../components/images/skyler.png';
+import './Footer.css'
+
 function Footer() {
     return(
         <div id='footer'>
-            <a href="https://github.com/sriveragithub" target="_blank"> <img src="https://lh3.googleusercontent.com/proxy/aqsCIffSQGUXC84WmBVJ8oXljuae287F_8kZLHBrR7k0heIc4o1sAU8zUWzUZ9ufacKWhQmF36pgbXzKNEXJzQfqSLICL-UL"></img> </a>
-            <a href="https://github.com/csolis1" target="_blank"> <img src="https://lh3.googleusercontent.com/proxy/aqsCIffSQGUXC84WmBVJ8oXljuae287F_8kZLHBrR7k0heIc4o1sAU8zUWzUZ9ufacKWhQmF36pgbXzKNEXJzQfqSLICL-UL"></img> </a>
-            <a href="https://github.com/myrriame" target="_blank"> <img src="https://lh3.googleusercontent.com/proxy/aqsCIffSQGUXC84WmBVJ8oXljuae287F_8kZLHBrR7k0heIc4o1sAU8zUWzUZ9ufacKWhQmF36pgbXzKNEXJzQfqSLICL-UL"></img> </a>
+            <a href="https://github.com/sriveragithub" target="_blank"> <img className="skyler" src={skyler}></img> </a>
+            <a href="https://github.com/csolis1" target="_blank"> <img className="celi" src={celi}></img> </a>
+            <a href="https://github.com/myrriame" target="_blank"> <img className="myr" src={myr}></img> </a>
         </div>
     )
 
