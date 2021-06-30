613de6863154200652dd7b85f6ee8c9d14e9bf28
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-06-30 18:37:57 -0400

    names rendered

--- client/src/games/chess/store.js
+++ client/src/games/chess/store.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
-// import { counter } from './reducers';
+import { playerReducer } from './reducers';
 
- const store = createStore(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+ const store = createStore(playerReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
  export default store
