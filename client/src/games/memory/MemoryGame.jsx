6758170f025e38adc7609f2c67e2f00cda602733
Eric Treadway <eric.s.treadway@gmail.com>
2021-06-29 19:16:36 -0500

    made scoring incrament fire when win condition is hit

--- client/src/games/memory/MemoryGame.jsx
+++ client/src/games/memory/MemoryGame.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Board from "./components/Board";
 import ScoreBoard from "./components/ScoreBoard";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 function MemoryGame() {
   return (
-    <div>
-      <h1>Hello from Memory Game! part 2</h1>
-      <Board />
-      <ScoreBoard />
-    </div>
+    <Provider store={store}>
+      <div>
+        <h1>Hello from Memory Game! part 2</h1>
+        <Board />
+        <ScoreBoard />
+      </div>
+    </Provider>
   );
 }
 
--- client/src/games/memory/containers/IncrementContainer.js
+++ client/src/games/memory/containers/IncrementContainer.js
@@ -1,15 +1,15 @@
-import Increment from '../components/Increment';
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
+import Board from "../components/Board";
 
-import { actionIncrement } from '../actions';
+import { actionIncrement } from "../actions";
 
 function mapDispatchToProps(dispatch) {
-    return {
-        // propName in React : a fn that calls dispatch
-        handleClick: () => {
-            dispatch(actionIncrement());
-        }
-    }
+  return {
+    // propName in React : a fn that calls dispatch
+    handleClick: () => {
+      dispatch(actionIncrement());
+    },
+  };
 }
 
-export default connect(null, mapDispatchToProps)(Increment);
+export default connect(null, mapDispatchToProps)(Board);
