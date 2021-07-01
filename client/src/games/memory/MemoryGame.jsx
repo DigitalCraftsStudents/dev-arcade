c52b441a05a24899c06017e83b0e903057b56c75
drlenny <66442932+drlenny@users.noreply.github.com>
2021-06-30 21:23:01 -0500

    fix issue with other pages

--- client/src/games/memory/MemoryGame.jsx
+++ client/src/games/memory/MemoryGame.jsx
@@ -3,11 +3,13 @@ import Board from "./components/Board";
 import ScoreBoard from "./components/ScoreBoard";
 import { store } from "./store";
 import { Provider } from "react-redux";
+import './components/style.css'
 
 function MemoryGame() {
+
   return (
     <Provider store={store}>
-      <div>
+      <div id="memory-game">
         <h1>Hello from Memory Game! part 2</h1>
         <Board />
         <ScoreBoard />
